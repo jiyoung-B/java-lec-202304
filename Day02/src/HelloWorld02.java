ca9811f8f21503aa601e778c99a533a2e223b12b
jy <catgirl0313@naver.com>
2023-04-28 22:02:29 +0900

    day2
    basic

---
+++ Day02/src/HelloWorld02.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld02 {
+	public static void main(String[] args) {
+		System.out.println("day2");
+
+	}
+
+
+}
---
+++ Day02/src/JavaStructure.java
@@ -0,0 +1,13 @@
+// 1. 자바는 클래스이름과 파일명이 같아야 합니다.
+// 2. 자바는 대소문자를 구분합니다.
+// 	ex) Java java -> 같지 않아요.
+// 3. { } 중괄호로 문단, 문장을 구분합니다.
+// 4. 자바의 문장의 끝은 ; 을 사용합니다.
+
+public class JavaStructure {
+	
+	public static void main(String[] args) {
+		System.out.println("실행하기");
+	}
+
+}
